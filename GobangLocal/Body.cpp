95b69f600c285b2ce37255ee2ad277e5e2c5c332
千年的霜雪 <1371684195@qq.com>
2024-06-11 17:31:11 +0800

    Update

---
+++ GobangLocal/Body.cpp
@@ -0,0 +1,133 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#define _WINSOCK_DEPRECATED_NO_WARNINGS 1
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <iostream>
+#include <stdio.h>
+#include <graphics.h>
+#include <cstring>
+
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+void DrawMap()
+{
+	line(200, 0, 200, 600);
+	line(400, 0, 400, 600);
+	line(0, 200, 600, 200);
+	line(0, 400, 600, 400);
+
+	line(0, 600, 600, 600);
+}
+
+int main()
+{
+	// 初始化画布与线型等 - 600 * 700
+	initgraph(600, 700);
+	LINESTYLE lsy;
+	getlinestyle(&lsy);
+	lsy.thickness = 10;
+	setlinestyle(&lsy);
+	setlinecolor(WHITE);
+
+	// 初始化winsock库
+	WSADATA data;
+	int test = 0;
+	test = WSAStartup(MAKEWORD(2, 2), &data);
+	if (test != 0)
+	{
+		perror("startup");
+		return -1;
+	}
+	cout << "startup" << endl;
+
+	// 创建用于通信的套接字
+	SOCKET fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (fd == -1)
+	{
+		perror("socket");
+		return -1;
+	}
+	cout << "socket" << endl;
+
+	// 连接服务器IP 端口
+	struct sockaddr_in saddr;
+	saddr.sin_family = AF_INET;
+	saddr.sin_port = htons(60001);
+	//saddr.sin_addr.S_un.S_addr = inet_addr("47.116.37.143");
+	inet_pton(AF_INET, "47.116.37.143", &saddr.sin_addr.S_un.S_addr);
+	int ret = connect(fd, (sockaddr*)&saddr, sizeof saddr);
+	if (ret == -1)
+	{
+		perror("connect");
+		return -1;
+	}
+	cout << "connect" << endl;
+
+	// 通信
+	char num[2];
+	char buff[100];
+	ExMessage message;
+	short relx = 0, rely = 0;
+	while (1)
+	{
+		//// 发送数据
+		//sprintf(buff, "Hello, %d\n", num++);
+		//send(fd, buff, strlen(buff) + 1, 0);
+
+		DrawMap();
+
+		// 接收数据
+		TCHAR waiting[] = _T("Waiting for the other to act.");
+		outtextxy(30, 650, waiting);
+		memset(buff, 0, sizeof buff);
+		memset(num, 0, sizeof num);
+		int len = recv(fd, num, sizeof num, 0); 
+		if (len > 0)
+		{
+			clearrectangle(0, 660, 600, 700);
+			if (buff[0] == '1') // 该信息需要打印
+			{
+				recv(fd, buff, sizeof buff, 0);
+				outtextxy(30, 650, buff);
+			}
+			else if (buff[0] == '0') // 该信息为游戏信息
+			{
+
+			}
+
+		
+		}
+		else if (len == 0)
+		{
+			cout << "Server break" << endl;
+			return -1;
+		}
+		else
+		{
+			perror("recv");
+			return -1;
+		}
+	
+		while (1) // 获取鼠标信息
+		{
+			message = getmessage();
+			if (message.message == WM_LBUTTONDOWN)
+			{
+				relx = message.x / 200;
+				rely = message.y / 200;
+				break;
+			}
+		}
+	}
+
+	// 关闭文件描述符
+	closesocket(fd);
+
+	// 释放套接字库
+	WSACleanup();
+
+	return 0;
+}
---
+++ GobangServer/GobangServerNew.c
@@ -0,0 +1,135 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h> // 包含了inet.h
+
+int main()
+{
+	// 连接玩家1
+
+	// 创建监听套接字
+	int fd1 = socket(AF_INET, SOCK_STREAM, 0);
+	if (fd1 == -1)
+	{
+		perror("socket");
+		return -1;
+	}
+
+	// 绑定本地IP端口 - 60001
+	struct sockaddr_in saddr1;
+	saddr1.sin_family = AF_INET;
+	saddr1.sin_port = htons(60001);
+	saddr1.sin_addr.s_addr = INADDR_ANY;
+	int ret1 = bind(fd1, (struct sockaddr*)&saddr1, sizeof saddr1);
+	if (ret1 == -1)
+	{
+		perror("bind");
+		return -1;
+	}
+
+	// 设置监听
+	ret1 = listen(fd1, 128);
+	if (ret1 == -1)
+	{
+		perror("listen");
+		return -1;
+	}
+
+	// 阻塞并等待客户端连接
+	struct sockaddr_in caddr1;
+	int addrlen1 = sizeof caddr1;
+	int cfd1 = accept(fd1, (struct sockaddr*)&caddr1, &addrlen1);
+	if (cfd1 == -1)
+	{
+		perror("accept");
+		return -1;
+	}
+
+	// 若连接成功，打印客户端的IP与端口信息
+	char ip1[32];
+	printf("1客户端IP: %s, 端口: %d\n", inet_ntop(AF_INET, &caddr1.sin_addr.s_addr, ip1, sizeof ip1), ntohs(caddr1.sin_port));
+
+	char answtype[2] = "1";
+	char answ[] = "Wait for Another player.";
+	int lenansw = sizeof answ;
+	int lenansw1 = sizeof answtype;
+	send(cfd1, answtype, lenansw1, 0);
+	send(cfd1, answ, lenansw, 0);
+
+	// 连接玩家2
+
+	// 创建监听套接字
+	int fd2 = socket(AF_INET, SOCK_STREAM, 0);
+	if (fd2 == -1)
+	{
+		perror("socket2");
+		return -1;
+	}
+
+	// 绑定本地IP端口 - 60001
+	struct sockaddr_in saddr2;
+	saddr2.sin_family = AF_INET;
+	saddr2.sin_port = htons(60001);
+	saddr2.sin_addr.s_addr = INADDR_ANY; 
+	int ret2 = bind(fd2, (struct sockaddr*)&saddr2, sizeof saddr2);
+	if (ret2 == -1)
+	{
+		perror("bind2");
+		return -1;
+	}
+
+	// 设置监听
+	ret2 = listen(fd2, 128);
+	if (ret2 == -1)
+	{
+		perror("listen2");
+		return -1;
+	}
+
+	// 阻塞并等待客户端连接
+	struct sockaddr_in caddr2;
+	int addrlen2 = sizeof caddr2;
+	int cfd2 = accept(fd2, (struct sockaddr*)&caddr2, &addrlen2);
+	if (cfd2 == -1)
+	{
+		perror("accept2");
+		return -1;
+	}
+
+	// 若连接成功，打印客户端的IP与端口信息
+	char ip2[32];
+	printf("2客户端IP: %s, 端口: %d\n", inet_ntop(AF_INET, &caddr2.sin_addr.s_addr, ip2, sizeof ip2), ntohs(caddr2.sin_port));
+
+
+	// 通信
+	while (1)
+	{
+		// 接收数据
+		char buff[1024];
+		int len = recv(cfd1, buff, sizeof buff, 0);
+		if (len > 0)
+		{
+			printf("client say: %s\n", buff);
+			send(cfd1, buff, len, 0);
+		} // 接收到数据
+		else if (len == 0)
+		{
+			break;
+		} // 客户端断开连接
+		else
+		{
+			perror("recv");
+			break;
+		} //调用失败
+	}
+
+
+	// 关闭文件描述符
+	close(fd1);
+	close(cfd1);
+	close(fd2);
+	close(cfd2);
+
+	return 0;
+}
