2454ed2f08ee5f4c610f655c5e12073237e5a96c
unknown <1371684195@qq.com>
2024-08-27 12:28:09 +0800

    update

--- Genshen/Genshen/main.cpp
+++ Genshen/Genshen/main.cpp
@@ -1,15 +1,17 @@
 #include <graphics.h>
 #include <string>
+#include <vector>
 
 int idx_current_anim = 0; // 存储当前动画帧索引
 const int PLAYER_ANIM_NUM = 6; // 动画帧总数常量
-const int PLAYER_SPEED = 3; // 玩家速度
+
+// 窗口尺寸
+const int WINDOW_WIDTH = 1280;
+const int WINDOW_HEIGHT = 720;
 
 // 动画存储数组
 IMAGE img_player_left[PLAYER_ANIM_NUM];
 IMAGE img_player_right[PLAYER_ANIM_NUM];
-
-POINT player_pos = { 500, 500 }; // 存储玩家位置
 
 // 支持透明度的渲染函数
 #pragma comment(lib, "MSIMG32.LIB")
@@ -20,8 +22,307 @@ inline void putimage_alpha(int x, int y, IMAGE* img)
 
 	AlphaBlend(GetImageHDC(NULL), x, y, w, h,
 		GetImageHDC(img), 0, 0, w, h, { AC_SRC_OVER, 0, 255, AC_SRC_ALPHA });
-
 }
+
+// 动画渲染类
+class Animation
+{
+public:
+	Animation(LPCTSTR path, int num, int interval) // 图片文件路径、动画使用的图片数量、动画帧间隔
+	{
+		interval_ms = interval;
+
+		// 加载图片
+		TCHAR path_file[256];
+		for (size_t i = 0; i < num; i++)
+		{
+			_stprintf_s(path_file, path, i);
+
+			IMAGE* frame = new IMAGE();
+			loadimage(frame, path_file);
+			frame_list.push_back(frame);
+		}
+	}
+
+	~Animation()
+	{
+		// 释放内存
+		for (size_t i = 0; i < frame_list.size(); i++)
+		{
+			delete frame_list[i];
+		}
+	}
+
+	// 动画渲染与更新
+	void Play(int x, int y, int delta) // x,y动画渲染位置 delta表示距上次调用过去多久
+	{
+		timer += delta;
+		if (timer >= interval_ms)
+		{
+			idx_frame = (idx_frame + 1) % frame_list.size();
+			timer = 0;
+		}
+
+		putimage_alpha(x, y, frame_list[idx_frame]);
+	}
+
+private:
+	int interval_ms = 0;
+	int idx_frame = 0; // 动画计时器
+	int timer = 0; // 动画帧索引
+	std::vector<IMAGE*> frame_list; // 动画帧序列
+};
+
+// 玩家类
+class Player
+{
+public:
+	Player()
+	{
+		loadimage(&img_shadow, _T("img/shadow_player.png"));
+		anim_left = new Animation(_T("img/player_left_%d.png"), 6, 45);
+		anim_right = new Animation(_T("img/player_right_%d.png"), 6, 45);
+	}
+
+	~Player()
+	{
+		delete anim_left;
+		delete anim_right;
+	}
+
+	// 处理玩家操作消息
+	void ProcessEvent(const ExMessage& msg)
+	{
+		switch (msg.message)
+		{
+		case WM_KEYDOWN:
+			switch (msg.vkcode)
+			{
+			case VK_UP:
+				is_move_up = true;
+				break;
+			case VK_DOWN:
+				is_move_down = true;
+				break;
+			case VK_LEFT:
+				is_move_left = true;
+				break;
+			case VK_RIGHT:
+				is_move_right = true;
+				break;
+			}
+			break;
+
+		case WM_KEYUP:
+			switch (msg.vkcode)
+			{
+			case VK_UP:
+				is_move_up = false;
+				break;
+			case VK_DOWN:
+				is_move_down = false;
+				break;
+			case VK_LEFT:
+				is_move_left = false;
+				break;
+			case VK_RIGHT:
+				is_move_right = false;
+				break;
+			}
+			break;
+		}
+	}
+
+	// 处理玩家移动
+	void Move()
+	{
+		// 使用方向向量处理速度
+		int dir_x = is_move_right - is_move_left; // x分量
+		int dir_y = is_move_down - is_move_up; // y分量
+		double len_dir = sqrt(dir_x * dir_x + dir_y * dir_y); // 单位向量长度
+		if (len_dir != 0)
+		{
+			double normalized_x = dir_x / len_dir;
+			double normalized_y = dir_y / len_dir;
+			position.x += (int)(SPEED * normalized_x);
+			position.y += (int)(SPEED * normalized_y);
+		}
+
+		// 限制玩家位置
+		if (position.x < 0) position.x = 0;
+		if (position.y < 0) position.y = 0;
+		if (position.x + PLAYER_WIDTH > WINDOW_WIDTH) position.x = WINDOW_WIDTH - PLAYER_WIDTH;
+		if (position.y + PLAYER_HEIGHT > WINDOW_HEIGHT) position.y = WINDOW_HEIGHT - PLAYER_HEIGHT;
+	}
+
+	// 绘制玩家
+	void Draw(int delta)
+	{
+		// 使阴影x方向居中，y方向位于玩家脚底
+		int pos_shadow_x = position.x + (PLAYER_WIDTH / 2 - SHADOW_WIDTH / 2); // 使阴影水平居中
+		int pos_shadow_y = position.y + PLAYER_HEIGHT - 8;
+		putimage_alpha(pos_shadow_x, pos_shadow_y, &img_shadow);
+
+		static bool facing_left = false;
+		int dir_x = is_move_right - is_move_left; // 玩家在x轴上移动的方向
+		if (dir_x < 0)
+			facing_left = true;
+		else if (dir_x > 0)
+			facing_left = false;
+
+		if (facing_left)
+			anim_left->Play(position.x, position.y, delta);
+		else
+			anim_right->Play(position.x, position.y, delta);
+	}
+
+	const POINT& GetPosition() const
+	{
+		return position;
+	}
+
+private:
+	const int SPEED = 3;
+	const int PLAYER_WIDTH = 80; // 玩家宽度
+	const int PLAYER_HEIGHT = 80; // 玩家高度
+	const int SHADOW_WIDTH = 32; // 阴影宽度
+
+private:
+	IMAGE img_shadow;
+	Animation* anim_left;
+	Animation* anim_right;
+	POINT position = { 500, 500 }; // 玩家位置
+	bool is_move_up = false;
+	bool is_move_down = false;
+	bool is_move_left = false;
+	bool is_move_right = false;
+
+};
+
+// 子弹类
+class Bullet
+{
+public:
+	POINT position = { 0, 0 };
+
+public:
+	Bullet() = default;
+	~Bullet() = default;
+
+	void Draw() const
+	{
+		setlinecolor(RGB(255, 155, 50));
+		setfillcolor(RGB(200, 75, 10));
+		fillcircle(position.x, position.y, RADIUS);
+	}
+
+private:
+	const int RADIUS = 10;
+};
+
+// 敌人类
+class Enemy
+{
+public:
+	Enemy()
+	{
+		loadimage(&img_shadow, _T("img/shadow_enemy.png"));
+		anim_left = new Animation(_T("img/enemy_left_%d.png"), 6, 45);
+		anim_right = new Animation(_T("img/enemy_right_%d.png"), 6, 45);
+
+		// 敌人出生边界
+		enum class SpawnEdge
+		{
+			Up = 0,
+			Down,
+			Left,
+			Right
+		};
+
+		// 将敌人放置在地图外边界的随机位置
+		SpawnEdge edge = (SpawnEdge)(rand() % 4);
+		switch (edge)
+		{
+		case SpawnEdge::Up:
+			position.x = rand() % WINDOW_WIDTH;
+			position.y = -FRAME_HEIGHT;
+			break;
+		case SpawnEdge::Down:
+			position.x = rand() % WINDOW_WIDTH;
+			position.y = WINDOW_HEIGHT;
+		case SpawnEdge::Left:
+			position.x = -FRAME_WIDTH;
+			position.y = rand() % WINDOW_HEIGHT;
+			break;
+		case SpawnEdge::Right:
+			position.x = WINDOW_WIDTH;
+			position.y = rand() % WINDOW_HEIGHT;
+			break;
+		default:
+			break;
+		}
+	}
+
+	// 检测与子弹发生碰撞
+	bool CheckBulletCollision(const Bullet& bullet)
+	{
+		return false;
+	}
+
+	// 检测与玩家发生碰撞
+	bool CheckPlayerCollision(const Player& player)
+	{
+		return false;
+	}
+
+	// 追踪玩家移动
+	void Move(const Player& player)
+	{
+		const POINT& player_position = player.GetPosition();
+		int dir_x = player_position.x - position.x;
+		int dir_y = player_position.y - position.y;
+		double len_dir = sqrt(dir_x * dir_x + dir_y * dir_y);
+		if (len_dir != 0)
+		{
+			double normalized_x = dir_x / len_dir;
+			double normalized_y = dir_y / len_dir;
+			position.x += (int)(SPEED * normalized_x);
+			position.y += (int)(SPEED * normalized_y);
+		}
+	}
+
+	// 绘制敌人
+	void Draw(int delta)
+	{
+		int pos_shadow_x = position.x + (FRAME_WIDTH / 2 - SHADOW_WIDTH / 2);
+		int pos_shadow_y = position.y + FRAME_HEIGHT - 35;
+		putimage_alpha(pos_shadow_x, pos_shadow_y, &img_shadow);
+
+		if (facing_left)
+			anim_left->Play(position.x, position.y, delta);
+		else
+			anim_right->Play(position.x, position.y, delta);
+	}
+
+	~Enemy()
+	{
+		delete anim_left;
+		delete anim_right;
+	}
+
+private:
+	const int SPEED = 2;
+	const int FRAME_WIDTH = 80;
+	const int FRAME_HEIGHT = 80;
+	const int SHADOW_WIDTH = 48;
+
+private:
+	IMAGE img_shadow;
+	Animation* anim_left;
+	Animation* anim_right;
+	POINT position = { 0, 0 };
+	bool facing_left = false;
+};
 
 // 加载动画
 void LoadAnimation()
@@ -39,93 +340,53 @@ void LoadAnimation()
 	}
 }
 
+// 生成新的敌人
+void TryGenerateEnemy(std::vector<Enemy*>& enemy_list)
+{
+	const int INTERVAL = 100;
+	static int counter = 0;
+	if ((++counter) % INTERVAL == 0)
+		enemy_list.push_back(new Enemy());
+}
+
 int main()
 {
 	initgraph(1280, 720); // 初始化窗口为1280*720
 
 	bool running = true; // 控制游戏是否进行
 
+	Player player; // 玩家实例
 	ExMessage msg;
 	IMAGE img_background; // 背景图
-
-	bool is_move_up = false;
-	bool is_move_down = false;
-	bool is_move_left = false;
-	bool is_move_right = false;
-
+	std::vector<Enemy*> enemy_list; // 敌人列表
+	
 	loadimage(&img_background, _T("img/background.png")); // 加载背景图
-	LoadAnimation();
 
 	BeginBatchDraw();
 
-	while(running)
+	while (running)
 	{
 		DWORD start_time = GetTickCount();
 
 		// 输入处理
 		while (peekmessage(&msg))
 		{
-			if (msg.message == WM_KEYDOWN)
-			{
-				switch (msg.vkcode)
-				{
-				case VK_UP:
-					is_move_up = true;
-					break;
-				case VK_DOWN:
-					is_move_down = true;
-					break;
-				case VK_LEFT:
-					is_move_left = true;
-					break;
-				case VK_RIGHT:
-					is_move_right = true;
-					break;
-				}
-			}
-			else if (msg.message == WM_KEYUP)
-			{
-				switch (msg.vkcode)
-				{
-				case VK_UP:
-					is_move_up = false;
-					break;
-				case VK_DOWN:
-					is_move_down = false;
-					break;
-				case VK_LEFT:
-					is_move_left = false;
-					break;
-				case VK_RIGHT:
-					is_move_right = false;
-					break;
-				}
-			}
+			player.ProcessEvent(msg);
 		}
 
 		// 数据处理
-		if (is_move_up)
-			player_pos.y -= PLAYER_SPEED;
-		if (is_move_down)
-			player_pos.y += PLAYER_SPEED;
-		if (is_move_left)
-			player_pos.x -= PLAYER_SPEED;
-		if (is_move_right)
-			player_pos.x += PLAYER_SPEED;
-
-		static int counter = 0; // 记录当前动画帧共播放几个游戏帧
-		if (++counter % 5 == 0) // 每5帧切换一次动画
-		{
-			idx_current_anim++;
-		}
-
-		idx_current_anim %= PLAYER_ANIM_NUM; // 使动画循环
+		player.Move();
+		TryGenerateEnemy(enemy_list);
+		for (Enemy* enemy : enemy_list)
+			enemy->Move(player);
 
 		// 渲染画面
 		cleardevice();
 
 		putimage(0, 0, &img_background);
-		putimage_alpha(player_pos.x, player_pos.y, &img_player_left[idx_current_anim]);
+		player.Draw(1000 / 144);
+		for (Enemy* enemy : enemy_list)
+			enemy->Draw(1000 / 144);
 
 		FlushBatchDraw();
 
