361f71bf07f29d515eb5182096f7c17e6435ff1f
unknown <1371684195@qq.com>
2024-09-12 22:36:08 +0800

    update

--- PlantsBattle/PlantsBattle/player.h
+++ PlantsBattle/PlantsBattle/player.h
@@ -4,10 +4,10 @@
 #include "vector2.h"
 #include "animation.h"
 #include "player_id.h"
+#include "platform.h"
 #include <graphics.h>
 
-#include <iostream>
-
+extern std::vector<Platform> platform_list;
 class Player
 {
 public:
@@ -26,6 +26,8 @@ public:
 		{
 			is_facing_right = direction > 0;
 			current_animation = is_facing_right ? &animation_run_right : &animation_run_left;
+			float distance = direction * run_velocity * delta;
+			on_run(distance);
 		}
 		else
 		{
@@ -33,6 +35,8 @@ public:
 		}
 
 		current_animation->on_update(delta);
+
+		move_and_collide(delta);
 	}
 
 	virtual void on_draw(const Camera& camera)
@@ -56,6 +60,9 @@ public:
 				case 0x44: // D
 					is_right_key_down = true;
 					break;
+				case 0x57: // W
+					on_jump();
+					break;
 				}
 				break;
 			case PlayerID::P2:
@@ -66,6 +73,9 @@ public:
 					break;
 				case	VK_RIGHT: // ->
 					is_right_key_down = true;
+					break;
+				case VK_UP: // ^
+					on_jump();
 					break;
 				}
 				break;
@@ -101,6 +111,19 @@ public:
 		}
 	}
 
+	virtual void on_run(float distance)
+	{
+		position.x += distance;
+	}
+
+	virtual void on_jump()
+	{
+		if (velocity.y != 0)
+			return;
+
+		velocity.y += jump_velocity;
+	}
+
 	void set_id(PlayerID id)
 	{
 		this->id = id;
@@ -113,7 +136,48 @@ public:
 	}
 
 protected:
+	// 物理相关
+	void move_and_collide(int delta)
+	{
+		velocity.y += gravity * delta;
+		position += velocity * (float)delta;
+
+		// 平台单向碰撞检测
+		if (velocity.y > 0)
+		{
+			for (const Platform& platform : platform_list)
+			{
+				const Platform::CollisionShape& shape = platform.shape;
+				bool is_collide_x = (max(position.x + size.x, shape.right) - min(position.x, shape.left)
+					<= size.x + (shape.right - shape.left));
+				bool is_collide_y = (shape.y >= position.y && shape.y <= position.y + size.y);
+
+				if (is_collide_x && is_collide_y)
+				{
+					float delta_pos_y = velocity.y * delta;
+					float last_tick_foot_pos_y = position.y + size.y - delta_pos_y;
+					
+					if (last_tick_foot_pos_y <= shape.y)
+					{
+						position.y = shape.y - size.y;
+						velocity.y = 0;
+
+						break;
+					}
+				}
+			}
+		}
+	}
+
+protected:
+	const float gravity = 1.6e-3f; // 玩家重力
+	const float run_velocity = 0.55f; // 跑动速度
+	const float jump_velocity = -0.85f; // 跳跃速度
+
+protected:
+	Vector2 size; // 角色尺寸
 	Vector2 position; // 角色位置
+	Vector2 velocity; // 角色速度
 
 	Animation animation_idle_left; // 朝左的默认动画
 	Animation animation_idle_right; // 朝右的默认动画
